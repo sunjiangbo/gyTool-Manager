06637ae7cae5b91385395e224a1da57922cb576b
lgy-7461 <linuxer@cafuc.edu.cn>
2016-04-27 16:31:12 +0800

    实现照片上传

---
+++ Web/ImgUpload.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class ImgUpload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Files.Count == 0)
+        {
+            
+            Response.Write("error");
+            Response.End();
+        }
+
+        HttpPostedFile file = Request.Files[0];
+        file.SaveAs(MapPath("\\upload\\") + file.FileName);
+        Response.Write("http://"  + Request.Url.Host + ":" + Request.Url.Port + "/upload/"+file.FileName);
+    }
+}
